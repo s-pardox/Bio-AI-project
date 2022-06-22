5d446d9ee8895bab10ece289340274cf1d47c89c
s-pardox <stefanopardini@inventati.org>
2022-06-22 17:33:24 +0200

    Defined trainer's and encoder's search space parameters, as reported in Bu et al.'s paper

--- TestOptimizer.py
+++ TestOptimizer.py
@@ -1,5 +1,7 @@
 # For example, create a random HPO by yourself
 import random
+
+# autogl/module/hpo/base.py
 from autogl.module.hpo.base import BaseHPOptimizer
 
 class TestOptimizer(BaseHPOptimizer):
@@ -11,11 +13,15 @@ class TestOptimizer(BaseHPOptimizer):
     # The most important thing you should do is completing optimization function
     def optimize(self, trainer, dataset, time_limit=None, memory_limit=None):
         # 1. Get the search space from trainer.
+        # http://mn.cs.tsinghua.edu.cn/autogl/documentation/docfile/tutorial/t_hpo.html#search-space
         space = trainer.hyper_parameter_space + trainer.model.hyper_parameter_space
+
         # optional: use self._encode_para (in BaseOptimizer) to pretreat the space
         # If you use _encode_para, the NUMERICAL_LIST will be spread to DOUBLE or INTEGER, LOG scaling type will be
         # changed to LINEAR, feasible points in CATEGORICAL will be changed to discrete numbers.
         # You should also use _decode_para to transform the types of parameters back.
+
+        # Ok, we can appreciate this 'facility' during a step-by-step run.
         current_space = self._encode_para(space)
 
         # 2. Define your function to get the performance.
@@ -33,48 +39,58 @@ class TestOptimizer(BaseHPOptimizer):
                 lr=hp["lr"],
                 KeyError: 'lr'
 
-            arranged_params = {
+            An example with real values:
+            rearranged_params = {
                 'trainer': {
                     'max_epoch': 101,
                     'early_stopping_round': 26,
                     'lr': 0.00011459309016222376,
-                    'weight_decay': 0.00972140339475499
+                    'weight_decay': 0.00        # (required) you need to set the trainer_hp_space properly.972140339475499
                 },
                 'encoder': {
                     'num_layers': 2,
-                    'hidden': [32],                         # Number of hidden units
+                    'hidden': [32],
                     'dropout': 0.5484903922544934,
-                    'act': 'elu'                            # Activation function
+                    'act': 'elu'
                 },
                 'decoder': {}
             }
             """
 
-            arranged_params = {
-                'trainer': {
-                    'max_epoch': para['max_epoch'],
-                    'early_stopping_round': para['early_stopping_round'],
-                    'lr': para['lr'],
-                    'weight_decay': para['weight_decay']
+            rearranged_params = {
+                'trainer': {                                                # Trainer's parameters
+                    'max_epoch': para['max_epoch'],                         # int
+                    'early_stopping_round': para['early_stopping_round'],   # int
+                    'lr': para['lr'],                                       # float
+                    'weight_decay': para['weight_decay']                    # float
                 },
-                'encoder': {
-                    'num_layers': para['num_layers'],
-                    'hidden': para['hidden'],               # Number of hidden units
-                    'dropout': para['dropout'],
-                    'act': para['act']                      # Activation function
+                'encoder': {                                                # NN (encoder) parameters
+                    'num_layers': para['num_layers'],                       # int
+                    'hidden': para['hidden'],                               # list (number of hidden units)
+                    'dropout': para['dropout'],                             # float
+                    'act': para['act']                                      # str (activation function)
                 },
                 'decoder': {}
             }
 
-            current_trainer = trainer.duplicate_from_hyper_parameter(para)
+            # current_trainer = trainer.duplicate_from_hyper_parameter(para)
+            # Accordingly with the inner method's behavior, we need to pass a rearranged data structure.
+
+            # The method returns a new instance of the trainer (e.g.: NodeClassificationFullTrainer, solver/classifier/
+            # node_classifier.py), also characterized by the parameters we're interested in, as well as by useful
+            # methods.
+            current_trainer = trainer.duplicate_from_hyper_parameter(rearranged_params)
             current_trainer.train(dset)
+
             loss, self.is_higher_better = current_trainer.get_valid_score(dset)
-            # For convenience, we change the score which is higher better to negative, then we should only minimize the score.
+            # For convenience, we change the score which is higher better to negative, then we should only minimize
+            # the score.
             if self.is_higher_better:
                 loss = -loss
             return current_trainer, loss
 
-        # 3. Define the how to get HP suggestions, it should return a parameter dict. You can use history trials to give new suggestions
+        # 3. Define the how to get HP suggestions, it should return a parameter dict.
+        # # You can use history trials to give new suggestions
         def get_random(history_trials):
             hps = {}
             for para in current_space:
@@ -91,15 +107,52 @@ class TestOptimizer(BaseHPOptimizer):
                     hps[para["parameterName"]] = random.choice(feasible_points)
             return hps
 
-        # 4. Run your algorithm. For each turn, get a set of parameters according to history information and evaluate it.
+        def get_evolutionary(history_trials):
+            hps = {}
+            for para in current_space:
+
+                # Because we use _encode_para function before, we should only deal with DOUBLE, INTEGER and DISCRETE
+                if para["type"] == "DOUBLE" or para["type"] == "INTEGER":
+                    hp = random.random() * (para["maxValue"] - para["minValue"]) + para["minValue"]
+                    if para["type"] == "INTEGER":
+                        hp = round(hp)
+                    hps[para["parameterName"]] = hp
+
+                elif para["type"] == "DISCRETE":
+                    feasible_points = para["feasiblePoints"].split(",")
+                    hps[para["parameterName"]] = random.choice(feasible_points)
+            return hps
+
+        # 4. Run your algorithm. For each turn, get a set of parameters according to history information and evaluate
+        # it.
         best_trainer, best_para, best_perf = None, None, None
         self.trials = []
+
+        """
+        ORIGINAL SOURCE CODE
+        
         for i in range(self.max_evals):
             # in this example, we don't need history trails. Since we pass None to history_trails
             new_hp = get_random(None)
-            # optional: if you use _encode_para, use _decode_para as well. para_for_trainer undos all transformation in _encode_para, and turns double parameter to interger if needed. para_for_hpo only turns double parameter to interger.
+            # optional: if you use _encode_para, use _decode_para as well. para_for_trainer undos all transformation
+            # in _encode_para, and turns double parameter to integer if needed. para_for_hpo only turns double
+            # parameter to integer.
             para_for_trainer, para_for_hpo = self._decode_para(new_hp)
+
+            # perf == loss
             current_trainer, perf = fn(dataset, para_for_trainer)
+        """
+        for i in range(self.max_evals):
+            # in this example, we don't need history trails. Since we pass None to history_trails
+            new_hp = get_random(None)
+            # optional: if you use _encode_para, use _decode_para as well. para_for_trainer undos all transformation
+            # in _encode_para, and turns double parameter to integer if needed. para_for_hpo only turns double
+            # parameter to integer.
+            para_for_trainer, para_for_hpo = self._decode_para(new_hp)
+
+            # perf == loss
+            current_trainer, perf = fn(dataset, para_for_trainer)
+
             self.trials.append((para_for_hpo, perf))
             if not best_perf or perf < best_perf:
                 best_perf = perf
--- test.py
+++ test.py
@@ -1,28 +1,3 @@
-"""
-import torch
-from autogl.datasets import build_dataset_from_name
-from autogl.solver import AutoNodeClassifier
-from autogl.module.train import Acc
-
-cora_dataset = build_dataset_from_name('cora')
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-solver = AutoNodeClassifier(
-    feature_module='deepgl',
-    graph_models=['gcn', 'gat'],
-    hpo_module='anneal',
-    ensemble_module='voting',
-    device=device
-)
-
-solver.fit(cora_dataset, time_limit=3600)
-
-predicted = solver.predict_proba()
-print('Test accuracy: ', Acc.evaluate(predicted,
-                                      cora_dataset.data.y[cora_dataset.data.test_mask].cpu().numpy()))
-"""
-
 import torch
 
 from autogl.datasets import build_dataset_from_name
@@ -33,16 +8,113 @@ from TestOptimizer import TestOptimizer
 dataset = build_dataset_from_name('cora')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# autoClassifier = AutoNodeClassifier()
+# Take a look to autogl/solver/base.py and, in particular, to node_classifier.py, in the same folder (the file contains
+# the AutoNodeClassifier class, that technically is labeled as 'solver').
 solver = AutoNodeClassifier(
-    # feature_module='deepgl',
-    # graph_models=['gcn', 'gat'],
+
+    # The (name of) the trainer used in this solver. Default to ``NodeClassificationFull``.
+    # Take a look to autogl/module/train/node_classification_full.py
+    default_trainer='NodeClassificationFull',
+
+    # We can bypass it, for the moment.
+    # feature_module = 'deepgl',
+
+    # graph_models = ['gcn', 'gat'],
     graph_models=['gcn'],
-    # hpo_module='anneal',
+
+    # hpo_module = 'anneal',
+    # Let's use our own HPO module :-)
     hpo_module=TestOptimizer(),
-    ensemble_module='voting',
+
+    # We can bypass it, for the moment.
+    # ensemble_module = 'voting',
+
     device=device,
-    max_evals=5
+    max_evals=5,
+
+    # https://autogl.readthedocs.io/en/latest/docfile/tutorial/t_hpo.html#search-space
+    # The following trainer's parameters (valued as the ones defined in Bu et al.'s paper) are passed to
+    # AutoNodeClassifier (node_classifier.py), overwriting the default ones.
+    trainer_hp_space=[
+        {
+            # In Bu et al.'s paper: P1 - continuous param in the [0.01,0,05] range.
+            'parameterName': 'lr',
+            'type': 'DOUBLE',
+            'maxValue': 1e-2,
+            'minValue': 5e-2,
+            'scalingType': 'LOG',
+        },
+        {
+            # In Bu et al.'s paper: P2 - continuous param in the [0.0001,0.001] range.
+            'parameterName': 'weight_decay',
+            'type': 'DOUBLE',
+            'maxValue': 1e-2,
+            'minValue': 1e-4,
+            'scalingType': 'LOG',
+        },
+        {
+            # In Bu et al.'s paper: P5 - discrete param in the [100,300] range.
+            'parameterName': 'max_epoch',
+            'type': 'INTEGER',
+            'maxValue': 300,
+            'minValue': 100,
+            'scalingType': 'LINEAR',
+        },
+        {
+            # In Bu et al.'s paper: P6 - discrete param in the [10,30] range.
+            'parameterName': 'early_stopping_round',
+            'type': 'INTEGER',
+            'maxValue': 30,
+            'minValue': 10,
+            'scalingType': 'LINEAR',
+        }
+    ],
+
+    # The following trainer's parameters (valued as the ones defined in Bu et al.'s paper) are passed to
+    # the GCN model (autogl/module/model/encoders/_dgl/_gcn.py), passing through the AutoNodeClassifier
+    # (node_classifier.py) solver, overwriting the default ones.
+    model_hp_spaces=[
+        # 'encoder'
+        [
+            {
+                # In Bu et al.'s paper: P3 - continuous param in the [0.2,0.8] range.
+                'parameterName': 'dropout',
+                'type': 'DOUBLE',
+                'maxValue': 0.8,
+                'minValue': 0.2,
+                'scalingType': 'LINEAR',
+            },
+            {
+                # This is exactly the default parameter defined in the GCN model itself.
+                'parameterName': 'num_layers',
+                'type': 'DISCRETE',
+                'feasiblePoints': '2,3,4',
+            },
+            {
+                # In Bu et al.'s paper: H1 - discrete param in the [4,16] range.
+                # TO CLARIFY: Does it refer to the Number of Hidden Units _per layer_?
+                # Yes, it does:
+                #   [ "GCN, the number of layers in the convolution structure was fixed, and only the number of units in
+                #   the hidden layer (H1) was adjusted." ]
+                'parameterName': 'hidden',
+                'type': 'NUMERICAL_LIST',
+                'numericalType': 'INTEGER',
+                'length': 3,
+                'minValue': [4, 4, 4],
+                'maxValue': [16, 16, 16],
+                'scalingType': 'LINEAR',
+                'cutPara': ('num_layers',),
+                'cutFunc': lambda x: x[0] - 1,
+            },
+            {
+                # In Bu et al.'s paper: P3 - discrete categorical param.
+                'parameterName': 'act',
+                'type': 'CATEGORICAL',
+                'feasiblePoints': ['leaky_relu', 'relu', 'elu', 'tanh'],
+            }
+        ]
+        # We don't have any 'decoder' counter-part parameters.
+    ]
 )
 
 # Split 0.2 of total nodes/graphs for train and 0.4 of nodes/graphs for validation,
@@ -53,10 +125,10 @@ solver = AutoNodeClassifier(
 solver.fit(dataset, time_limit=120)
 
 # get current leaderboard of the solver
-lb = solver.get_leaderboard()
+# lb  =  solver.get_leaderboard()
 # show the leaderboard info
-lb.show()
+# lb.show()
 
 acc = solver.evaluate(metric='acc')
 
-print("test acc: {:.4f}".format(acc))
+print('\ntest acc: {:.4f}'.format(acc))
