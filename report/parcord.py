248d4f628a8425b22e15605f482487667762c195
s-pardox <stefanopardini@inventati.org>
2022-08-30 11:07:32 +0200

    Refactoring

--- report/parcord.py
+++ report/parcord.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 df_original = pd.read_csv('Bio-AI_runs.csv')
 
-df_final = df_original.loc[df_original.groupby("group_name")["Test_Acc"].idxmax()]
+df_final = df_original.loc[df_original.groupby('group_name')['Test_Acc'].idxmax()]
 df_final.drop('num_layers', axis=1, inplace=True)
 
 df = df_final.copy()
@@ -13,9 +13,11 @@ categorical_columns = ['group_name', 'act']
 col_list = []
 
 for col in df.keys():
-    if col in categorical_columns:  # categorical columns
+    if col in categorical_columns:
+        # Categorical columns.
         values = df[col].unique()
-        value2dummy = dict(zip(values, range(len(values))))  # works if values are strings, otherwise we probably need to convert them
+        # Works if values are strings, otherwise we probably need to convert them.
+        value2dummy = dict(zip(values, range(len(values))))
         df[col] = [value2dummy[v] for v in df[col]]
         col_dict = dict(
             label=col,
@@ -23,7 +25,8 @@ for col in df.keys():
             ticktext=list(value2dummy.keys()),
             values=df[col],
         )
-    else:  # continuous columns
+    else:
+        # Continuous columns.
         col_dict = dict(
             range=(df[col].min(), df[col].max()),
             label=col,
@@ -31,12 +34,11 @@ for col in df.keys():
         )
     col_list.append(col_dict)
 
-
 fig = go.Figure(data=go.Parcoords(
-    line = dict(color = df_final['Test_Acc'],
-                    colorbar={'title':'Accuracy'},
-                    showscale=True),
+    line=dict(color=df_final['Test_Acc'],
+              colorbar={'title': 'Accuracy'},
+              showscale=True),
     dimensions=col_list))
 
-#fig.write_image("Parallel-Plot_CORA-GCN.png")
+# fig.write_image('Parallel-Plot_CORA-GCN.png')
 fig.show()
